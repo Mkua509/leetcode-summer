550b7beb81544e691765f02396a5c8e29d4d812c
Mkua509 <maternuskuang@gmail.com>
2026-01-24 21:09:09 +1300

    Completed 268

---
+++ 2026-1/2026-1-24/268.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        result = len(nums)
+
+        for i in range(len(nums)):
+            result += i - nums[i]
+        
+        return result

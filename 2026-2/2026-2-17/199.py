c3ebc375de2b61590f31bc20e51587f3a71be65a
Mkua509 <maternuskuang@gmail.com>
2026-02-17 21:10:52 +1300

    Completed 199

---
+++ 2026-2/2026-2-17/199.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+
+        queue = []
+        result = []
+
+        if root is None:
+            return result
+        
+        queue = deque()
+        queue.append(root)
+
+        while queue:
+            level_size = len(queue)
+            for i in range(level_size):
+                current_node = queue.popleft()
+                if i == (level_size - 1):
+                    result.append(current_node.val)
+                if current_node.left:
+                    queue.append(current_node.left)
+                if current_node.right:
+                    queue.append(current_node.right)
+        
+        return result
